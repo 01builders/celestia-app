c616cb867ee91fb1adca3da019fa8b608e845c72
Matt Kocubinski <mkocubinski@gmail.com>
2025-02-01 07:02:44 -0600

    fix min_fee_test.go errors

--- app/ante/fee_checker.go
+++ app/ante/fee_checker.go
@@ -62,7 +62,7 @@ func ValidateTxFee(ctx context.Context, tx transaction.Tx, paramKeeper params.Ke
 		return nil, 0, errors.Wrap(sdkerror.ErrLogic, "failed to get app version")
 	}
 
-	if appVersion > v1.Version && sdkCtx.BlockHeight() > 0 {
+	if appVersion > v1.Version {
 		subspace, exists := paramKeeper.GetSubspace(minfee.ModuleName)
 		if !exists {
 			return nil, 0, errors.Wrap(sdkerror.ErrInvalidRequest, "minfee is not a registered subspace")

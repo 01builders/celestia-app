4abf84cd92a4d3d79ff8f84a1de6b5c837a7c1d1
chatton <github.qpeyb@simplelogin.fr>
2025-03-12 12:20:38 +0000

    chore: use authority field as signer, clean up test interface

--- x/blob/keeper/keeper.go
+++ x/blob/keeper/keeper.go
@@ -2,8 +2,8 @@ package keeper
 
 import (
 	"context"
-	"cosmossdk.io/errors"
 
+	"cosmossdk.io/errors"
 	storetypes "cosmossdk.io/store/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
